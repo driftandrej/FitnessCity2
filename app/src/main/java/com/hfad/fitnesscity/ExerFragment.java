5ece319e7daf7ad5050f1a3ed147fbbb8fcf57c9
admin <driftandrejnexus@gmail.com>
2019-03-27 14:33:19 +0200

    fef2

---
+++ app/src/main/java/com/hfad/fitnesscity/ExerFragment.java
@@ -0,0 +1,29 @@
+package com.hfad.fitnesscity;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ExerFragment extends Fragment {
+
+
+    public ExerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exer, container, false);
+    }
+
+}
